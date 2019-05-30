5995ea969e9891117651f811b670bc42939799ba
Josiah Parappally <josiahparappally@gmail.com>
2019-05-30 08:51:06 -0400

    Update bot.py
    Added 10 xpath capabilities.

--- bot.py
+++ bot.py
@@ -9,6 +9,18 @@ def order(keys):
     driver.get(keys["product_url"])
 
     driver.find_element_by_xpath('')
+    # going to need to be able to find at least 10 elements on the page
+    # use chrome developer tools incase headings on site change in the future
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
+    driver.find_element_by_xpath('')
 
 if __name__ == '__main':
     order(personalKeys)
