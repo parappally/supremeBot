73c6e6ecc5c43acc7b909a8a41174dd9bdd97fb4
Josiah Parappally <josiahparappally@gmail.com>
2019-05-31 08:07:52 -0400

    Update bot.py
    Added dropdown click capabilities

--- bot.py
+++ bot.py
@@ -18,9 +18,19 @@ def order(keys):
     driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys["address"]) # address in checkout
     # I don't have a unit number, so I skipped this step...
     driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys["zip"])
+    driver.find_element_by_xpath('//*[@id="order_billing_country"]/option[2]').click() # country dropdown
+    driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[10]').click() # province dropdown
     driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys["city"])
+
+
     driver.find_element_by_xpath('//*[@id="nnaerb"]').send_keys(keys["card_number"])
+    driver.find_element_by_xpath('//*[@id="order_billing_country"]/option[2]').click() # credit card expiry month dropdown
+    driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[10]').click() # credit card expiry year dropdown
     driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(keys["cvv"])
+
+
+    driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p[2]/label/div/ins')click() #terms and conditions
+    driver.find_element_by_xpath('//*[@id="pay"]/input')click() # process payment button
 
 if __name__ == '__main':
     order(personalKeys)
