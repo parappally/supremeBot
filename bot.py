f164dda4390b952ec0547ce721c28d96ea2de9c6
Josiah Parappally <josiahparappally@gmail.com>
2019-05-31 08:40:38 -0400

    Update bot.py
    Added dictionary for credit card month of expiry

--- bot.py
+++ bot.py
@@ -85,6 +85,21 @@ web_states = {
     "WY" : 60,
 }
 
+web_credit_card-month {
+    "Jan" : 1,
+    "Feb" : 2,
+    "Mar" : 3,
+    "Apr" : 4,
+    "May" : 5,
+    "Jun" : 6,
+    "Jul" : 7,
+    "Aug" : 8,
+    "Sep" : 9,
+    "Oct" : 10,
+    "Nov" : 11,
+    "Dec" : 12,
+}
+
 def order(keys):
 
     driver = webdriver.Chrome('./chromedriver')
