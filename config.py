3484cd3a3c804a29bc2066cd99c1b64838c71e71
Josiah Parappally <josiahparappally@gmail.com>
2019-05-31 08:49:32 -0400

    Update config.py
    Changed type(telephone) to int from string.

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ personalKeys = {
     "product_url" : "https://www.supremenewyork.com/shop/tops-sweaters/rkdjagw4r/nr9mdzfwv",
     "name" : "Josiah Parappally",
     "email" : "josiahparappally@gmail.com",
-    "telephone" : "647-824-9936",
+    "telephone" : 6478249936,
     "address" : "1845 Willow Way",
     "zip" : "L5M 4Y5",
     "city" : "Mississauga",
-    "telephone" : 6478249936,
     "card_number" : 5555555555554444, #type : random mastercard from google
     "cvv" : 543
 }
