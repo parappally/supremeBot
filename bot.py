a40db39ea1997f8d90092173355b078e6b1cfe6f
Josiah Parappally <josiahparappally@gmail.com>
2019-06-01 10:05:59 -0400

    Update bot.py
    Removed old code

--- bot.py
+++ bot.py
@@ -123,43 +123,6 @@ def timeme(method):
         return result
     return wrapper
 
-
-# @timeme
-# def order(keys):
-#
-#     driver = webdriver.Chrome('./chromedriver')
-#
-#     driver.get(keys["product_url"])
-#
-#     # use chrome developer tools incase headings on site change in the future.. use copy by xpath
-#     driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
-#     time.sleep(1) # going to test if this can be lowered
-#     driver.find_element_by_xpath('//*[@id="cart"]/a[2]').click()
-#     driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(keys["name"]) #name in checkout
-#     driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(keys["email"]) #email in checkout
-#     driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(keys["telephone"]) # telephone number in checkout
-#     driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys["address"]) # address in checkout
-#     # I don't have a unit number, so I skipped this step...
-#     driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys["zip"])
-#
-#     driver.find_element_by_xpath('//*[@id="order_billing_country"]/option[web_countries["CA"]]').click() # country dropdown
-#     driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[web_provinces["ON"]]').click() # province dropdown
-#     # driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[web_states["AS"]]').click() # state dropdown UNCOMMENT THIS AND COMMENT OUT PROVINCE IF YOU LIVE IN USA
-#     driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys["city"])
-#
-#
-#     driver.find_element_by_xpath('//*[@id="nnaerb"]').send_keys(keys["card_number"])
-#     driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[web_credit_card_month["June"]]').click() # credit card expiry month dropdown, change month to corresponding month
-#     driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[web_credit_card_year[2019]]').click() # credit card expiry year dropdown, change year to corresponding year
-#     driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(keys["cvv"])
-#
-#
-#     driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p[2]/label/div/ins').click() #terms and conditions
-#     driver.find_element_by_xpath('//*[@id="pay"]/input').click() # process payment button
-#
-# if __name__ == '__main':
-#     order(personalKeys)
-
 @timeme
 def order():
     # add to cart
