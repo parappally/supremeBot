f1a94ca501d3d3037f286295654d08149a2223d8
Josiah Parappally <josiahparappally@gmail.com>
2019-06-01 09:36:08 -0400

    Fixing supreme bot for my mac

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ personalKeys = {
     "product_url" : "https://www.supremenewyork.com/shop/tops-sweaters/rkdjagw4r/nr9mdzfwv", # insert specific product url here
     "name" : "Josiah Parappally",
     "email" : "josiahparappally@*****.com",
-    "telephone" : 9999999999 # needs to be a nat, not a string (no hyphens)
+    "telephone" : 9999999999, # needs to be a nat, not a string (no hyphens)
     "address" : "2014 Forest Hills Drive",
     "zip" : "B4N 4N4",
     "city" : "Mississauga",
-    "card_number" : 5555555555555555 #type : random mastercard from google
-    "cvv" : 555
+    "card_number" : 5555555555555555, #type : random mastercard from google
+    "cvv" : 555,
+    "apt" : 555 # unnecessary if you don't live in an apt
 }
