1eb15798a88662245daed201d114ac2d2fbbb584
Josiah Parappally <josiahparappally@gmail.com>
2019-06-01 10:05:32 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 personalKeys = {
     "product_url" : "https://www.supremenewyork.com/shop/pants/gmgf3d097/z1dwc437u", # insert specific product url here
-    "name" : "Josiah Parappally",
-    "email" : "josiahparappally@*****.com",
+    "name" : "Josiah Parappally", # insert your name here
+    "email" : "josiahparappally@gmail.com", # insert your email here
     "telephone" : 9999999999, # needs to be a nat, not a string (no hyphens)
-    "address" : "2014 Forest Hills Drive",
-    "zip" : "B4N 4N4",
-    "city" : "Mississauga",
+    "address" : "2014 Forest Hills Drive", # insert your address here
+    "zip" : "B4N 4N4", # insert your zip code here
+    "city" : "Mississauga", # insert your city here
     "card_number" : 5555555555555555, #type : random mastercard from google
-    "cvv" : 555,
+    "cvv" : 555, # insert your credit card cvv here
     "apt" : 555 # unnecessary if you don't live in an apt
 }
